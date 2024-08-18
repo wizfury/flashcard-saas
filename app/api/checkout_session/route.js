8678545bc24346733b7e059159af45795f7a09b4
@nd2049 <nd2049@hw.ac.uk>
2024-08-18 23:07:29 +0100

    modified: file

--- app/api/checkout_session/route.js
+++ app/api/checkout_session/route.js
@@ -2,14 +2,13 @@ import {NextResponse} from 'next/server'
 
 import Stripe from 'stripe';
 
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 const formatAmountForStripe = (amount,currency)=>{
     return Math.round(amount * 100)
 }
 
 export async function POST(req){
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
-
     
 const params= {
   mode: 'subscription',
@@ -40,4 +39,22 @@ const checkoutSession = await stripe.checkout.sessions.create(params);
 return NextResponse.json(checkoutSession,{
     status: 200
 })
+}
+
+export async function GET(req) {
+  const searchParams = req.nextUrl.searchParams
+  const session_id = searchParams.get('session_id')
+
+  try {
+    if (!session_id) {
+      throw new Error('Session ID is required')
+    }
+
+    const checkoutSession = await stripe.checkout.sessions.retrieve(session_id)
+
+    return NextResponse.json(checkoutSession)
+  } catch (error) {
+    console.error('Error retrieving checkout session:', error)
+    return NextResponse.json({ error: { message: error.message } }, { status: 500 })
+  }
 }
